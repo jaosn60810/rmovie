9baeb2a5122835a6c4c3c109a8ef4a4968617942
jason60810 <jason60810@gmail.com>
2023-02-13 22:22:04 +0800

    feat: add CORS anywhere

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import MovieCard from './MovieCard';
 import SearchIcon from './assets/search.svg';
 import './App.css';
 
-const API_URL = 'http://www.omdbapi.com?apikey=b6003d8a';
+const API_URL =
+  'https://web-production-f84d.up.railway.app/http://www.omdbapi.com?apikey=b6003d8a';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
